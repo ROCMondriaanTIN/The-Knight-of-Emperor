e9dd6f8726943e51482ab9da2d09519fe2c3e774
Slightly-Greg <302832548@student.rocmondriaan.nl>
2025-06-29 15:42:06 +0200

    trigger bonus level
    added a externa; resource correctly to trigger the bonus level

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,7 +8,9 @@ var score = 0
 func add_point():
 	score += 1
 	score_label.text = "You Collected " + str(score) + " coins."
+	print("Score: ", score)  # Debug print
 	if score == 16:
+		print("Attempting to change to bonus level...")  # Debug print
 		get_tree().change_scene_to_file("res://scenes/bonus_level.tscn")
 	
 	
